7f95945ae911c10649585a9376dc5378d0131fc2
Paul Hauser <pmh35@pitt.edu>
2016-10-13 23:29:17 -0400

    added some experience

--- scripts/script.js
+++ scripts/script.js
@@ -19,9 +19,11 @@ $(function(){
       if(winSize > 720) {
         responsiveLi.css("float", "right");
         responsiveLi.css("display", "inline-block");
+        $("body").css("padding", "0 15%");
         toggle = 0;
       } else {
         responsiveLi.css("display", "none")
+        $("body").css("padding", "0");
       }
 
   });
